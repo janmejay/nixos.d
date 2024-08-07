9a58052c2d4b166740dbd9dc02570f9e1122dbe2
Janmejay Singh <singh.janmejay@gmail.com>
2024-08-07 22:20:19 +0530

    try to add keyd

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./keyd.nix
     ];
   nixpkgs.config.allowUnfree = true;
   nix.package = pkgs.nixFlakes;
