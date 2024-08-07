8d914143b06f0494f1aa54a31b4c2d4791dd58a6
Janmejay Singh <singh.janmejay@gmail.com>
2024-08-07 23:00:58 +0530

    try to inject nixpkgs#cairo as a dependency

--- pkgs/warpd/default.nix
+++ pkgs/warpd/default.nix
@@ -5,35 +5,42 @@
   cmake,
   pkg-config,
 }:
-stdenv.mkDerivation rec {
-  pname = "warpd";
-  version = "1.3.5";
+let
+  nixpkgs = import <nixpkgs> { };
 
-  src = fetchFromGitHub {
-    owner = "rvaiya";
-    repo = "warpd";
-    rev = "v" + version;
-    hash = "sha256-YHTQ5N4SZSa3S3sy/lNjarKPkANIuB2khwyOW5TW2vo=";
+  inherit (nixpkgs) cairo;
+
+  warpd = stdenv.mkDerivation rec {
+    pname = "warpd";
+    version = "1.3.5";
+
+    src = fetchFromGitHub {
+      owner = "rvaiya";
+      repo = "warpd";
+      rev = "v" + version;
+      hash = "sha256-YHTQ5N4SZSa3S3sy/lNjarKPkANIuB2khwyOW5TW2vo=";
+    };
+
+    postPatch = ''
+      substituteInPlace Makefile \
+        --replace DESTDIR= DESTDIR=${placeholder "out"} \
+        --replace /usr ""
+    '';
+
+    buildInputs = [cairo];
+
+    enableParallelBuilding = true;
+
+    postInstall = ''
+      rm -rf $out/etc
+    '';
+
+    meta = with lib; {
+      description = "A modal keyboard driven interface for mouse manipulation.";
+      license = licenses.mit;
+      maintainers = with maintainers; [peterhoeg];
+      platforms = platforms.linux;
+    };
   };
-
-  postPatch = ''
-    substituteInPlace Makefile \
-      --replace DESTDIR= DESTDIR=${placeholder "out"} \
-      --replace /usr ""
-  '';
-
-  buildInputs = [cairo];
-
-  enableParallelBuilding = true;
-
-  postInstall = ''
-    rm -rf $out/etc
-  '';
-
-  meta = with lib; {
-    description = "A modal keyboard driven interface for mouse manipulation.";
-    license = licenses.mit;
-    maintainers = with maintainers; [peterhoeg];
-    platforms = platforms.linux;
-  };
-}
+in
+warpd
