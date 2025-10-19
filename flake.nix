0ccd62d022eea94069e006f41a64c6232f1cc1b0
Janmejay Singh <singh.janmejay@gmail.com>
2025-10-19 23:55:17 +0530

    First step to potentially merging home-mgr config across Linux and OSX

--- flake.nix
+++ flake.nix
@@ -15,10 +15,28 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      
+      linux_features = {
+        emacs = true;
+        zsh = true;
+        osx = false;
+        sops = true;
+        squid = true;
+        nixvim = false;
+      };
+
+      osx_features = {
+        emacs = false;
+        zsh = true;
+        osx = true;
+        sops = true;
+        nixvim = true;
+        squid = false;
+      };
 
       home-mgr-cfg = home-manager.lib.homeManagerConfiguration {
           pkgs = pkgs;
-          extraSpecialArgs = { inherit inputs; };
+          extraSpecialArgs = { features = linux_features; inherit inputs; };
           modules = [
             ./home-manager/home.nix
           ];
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,6 @@
-{ config, pkgs, inputs, lib, ... }:
+{ config, pkgs, inputs, lib, ... }@args:
 let
+  features = args.features;
   warpd = pkgs.callPackage ../pkgs/warpd {};
   hackery = pkgs.callPackage ../pkgs/hackery {};
   find-cursor = pkgs.callPackage ../pkgs/find-cursor {};
@@ -11,98 +12,122 @@ let
     ref = "master";
   };
   user="janmejay";
+  user_home = if features.osx then /Users/${user} else /home/${user};
 in {
   home.username = user;
-  home.homeDirectory = /home/${user};
+  home.homeDirectory = user_home;
   home.stateVersion = "24.05";
   nixpkgs = {
     config = {
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
-      permittedInsecurePackages = [
-        "squid-6.12"
-      ];
+      permittedInsecurePackages = if features.osx then [ "squid-6.12" ] else [];
     };
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  home.packages = with pkgs; [
-    # local (custom) v
-    warpd
-    hackery
-    copyq
-    find-cursor
+  home.packages = with pkgs; if features.osx then 
+      [ 
+        sops 
+        rustup
+        eza
+        bat
+        jq
+        ripgrep
+        cascadia-code
+        dejavu_fonts
+        powerline-fonts
+        ubuntu-sans-mono
+        intel-one-mono
+        source-code-pro
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        tree
+      ]
+    else 
+      [
+        # local (custom) v
+        warpd
+        hackery
+        copyq
+        find-cursor
 
-    # public v
-    patchelf
-    sysstat
-    timg
-    xorg.xkill
-    moreutils
-    sops
-    pistol
-    tcpdump
-    gcc
-    nix-index
-    graphviz
-    vscode-fhs
-    (jetbrains.plugins.addPlugins jetbrains.idea-community [ "github-copilot" ])
-    squid
-    dig
-    bc
-    rlwrap
-    xautolock
-    i3lock
-    zoom-us
-    pavucontrol
-    wireplumber
-    networkmanagerapplet
-    xclip
-    xfce.xfce4-screenshooter
-    xfce.thunar
-    nomacs # image viewer
-    gnumake
-    bat
-    fzf
-    silver-searcher
-    jq
-    tree
-    eza
-    git
-    firefox
-    google-chrome
-    tmux
-    python3
-    lsof
-    gdb
-    bash
-    cascadia-code
-    dejavu_fonts
-    powerline-fonts
-    ubuntu-sans-mono
-    intel-one-mono
-    source-code-pro
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    gimp
-    feh
-    pstree
-    ffmpeg
-    vlc
-    file
-    openjdk
-    nodejs
-    neovim
-    fd
-    ripgrep
-  ];
+        # public v
+        patchelf
+        sysstat
+        timg
+        xorg.xkill
+        moreutils
+        sops
+        pistol
+        tcpdump
+        gcc
+        nix-index
+        graphviz
+        vscode-fhs
+        (jetbrains.plugins.addPlugins jetbrains.idea-community [ "github-copilot" ])
+        squid
+        dig
+        bc
+        rlwrap
+        xautolock
+        i3lock
+        zoom-us
+        pavucontrol
+        wireplumber
+        networkmanagerapplet
+        xclip
+        xfce.xfce4-screenshooter
+        xfce.thunar
+        nomacs # image viewer
+        gnumake
+        bat
+        fzf
+        silver-searcher
+        jq
+        tree
+        eza
+        git
+        firefox
+        google-chrome
+        tmux
+        python3
+        lsof
+        gdb
+        bash
+        cascadia-code
+        dejavu_fonts
+        powerline-fonts
+        ubuntu-sans-mono
+        intel-one-mono
+        source-code-pro
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        gimp
+        feh
+        pstree
+        ffmpeg
+        vlc
+        file
+        openjdk
+        nodejs
+        neovim
+        fd
+        ripgrep
+      ];
 
   home.sessionVariables = {
     EDITOR="nvim";
@@ -112,37 +137,43 @@ in {
     c = "bat";
   };
 
-  home.file = {
-    ".config" = {
-      source = ../dots/dot_config;
-      recursive = true;
-    };
-    ".gitconfig".source = ../dots/gitconfig;
-    ".dev_utils".source = dev-utils;
-    ".jq".source = "${dev-utils}/rc/jq";
-    ".tmux.conf".source = ../dots/tmux.conf;
-    ".Xdefaults".source = ../dots/Xdefaults;
-    ".my.emacs.d".source = builtins.fetchGit {
-      url = "https://github.com/janmejay/emacs";
-      rev = "a0b42cac2241928ba054f660559e2ac9e068b7b1";
-      ref = "nixos";
-    };
-    ".emacs".source = ../dots/emacs;
-    "projects/rubrik/squid.d/run.sh" = {
-      source = ../dots/squid.run.sh;
-      executable = true;
-    };
-    "projects/rubrik/squid.d/conf/squid.conf" = {
-      source = ../dots/squid.conf;
-    };
-  };
+  home.file = lib.mkMerge [
+    {
+      ".config" = {
+        source = ../dots/dot_config;
+        recursive = true;
+      };
+      ".gitconfig".source = ../dots/gitconfig;
+      ".dev_utils".source = dev-utils;
+      ".jq".source = "${dev-utils}/rc/jq";
+      ".tmux.conf".source = ../dots/tmux.conf;
+    }
+    (if features.emacs then {
+       ".my.emacs.d".source = builtins.fetchGit {
+         url = "https://github.com/janmejay/emacs";
+         rev = "a0b42cac2241928ba054f660559e2ac9e068b7b1";
+         ref = "nixos";
+       };
+       ".emacs".source = ../dots/emacs;
+    } else {})
+    (if features.osx then {} else { ".Xdefaults".source = ../dots/Xdefaults; }) 
+    (if features.squid then {
+       "projects/rubrik/squid.d/run.sh" = {
+         source = ../dots/squid.run.sh;
+         executable = true;
+       };
+       "projects/rubrik/squid.d/conf/squid.conf" = {
+         source = ../dots/squid.conf;
+       };
+    } else {})
+  ];
 
   fonts.fontconfig.enable = true;
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    initExtra = ''
+    initContent = ''
     source ~/.dev_utils/rc/shared_shell_config
     DEFAULT_USER=${user}
     prompt_context() {
